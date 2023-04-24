e8da2ee237d890d85f832d82815bcb1ef0b82296
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-04-24 10:44:18 -0300

    add banner component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import Banner from "./Components/Banner";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-<div>teste</div>
+    <BrowserRouter>
+        <Banner />
+    </BrowserRouter>
 );
 
