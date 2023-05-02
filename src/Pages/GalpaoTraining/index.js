cfd5a71eccbfd87a2802d5b39362520a50017a73
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-05-02 10:03:49 -0300

    add taining section

--- src/Pages/GalpaoTraining/index.js
+++ src/Pages/GalpaoTraining/index.js
@@ -1,7 +1,60 @@
-
+import style from "./GalpaoTraining.css"
 
 export default function GalpaoTraining() {
     return (
-        <div>deu certo</div>
+
+        <section>
+            <h2 className="training-tittle">Treinamento Lasa</h2>
+
+        <div className="d-flex justify-content-center">
+            <div id="carouselExampleFade" className="carousel slide carousel-fade carousel-training" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="./assets/img/Treinamento-Lasa/00000.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00001.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00002.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00003.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00004.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00005.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00006.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00007.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00008.jpg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Treinamento-Lasa/00009.jpg" className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Voltar</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Avançar</span>
+            </button>
+        </div>
+        </div>
+        </section>
+
+        
+
+        
+
     )
 }
