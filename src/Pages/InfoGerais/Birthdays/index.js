c7486d501c847f93c7b9c1358b38d4a6dbcf2822
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-05-15 16:22:04 -0300

    alt aniversários

--- src/Pages/InfoGerais/Birthdays/index.js
+++ src/Pages/InfoGerais/Birthdays/index.js
@@ -15,17 +15,24 @@ export default function Birthdays() {
 
                     <div className="d-flex justify-content-center div-secundaria">
                     <div className="d-flex flex-column">
+                    <div className="d-flex anv">
+                            <div className="nome-anv d-flex">
+                                <span>Marcelo Cordovil</span>
+                                <span> - 25/05</span>
+                            </div>
+                        </div>
+
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Fábio Motta</span>
-                                <span> - 25/05/1979</span>
+                                <span> - 25/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Bruno da Silva</span>
-                                <span> - 21/05/1987</span>
+                                <span> - 21/05</span>
 
                             </div>
                         </div>
@@ -33,7 +40,7 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Arnaldo</span>
-                                <span> - 10/05/1971</span>
+                                <span> - 10/05</span>
 
                             </div>
                         </div>
@@ -41,7 +48,7 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Araújo</span>
-                                <span> - 16/05/1984</span>
+                                <span> - 16/054</span>
 
                             </div>
                         </div>
@@ -49,7 +56,7 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Figueiredo</span>
-                                <span> - 22/05/1986</span>
+                                <span> - 22/05</span>
 
                             </div>
                         </div>
@@ -57,7 +64,7 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Isaias</span>
-                                <span> - 31/05/1961</span>
+                                <span> - 31/05</span>
 
                             </div>
                         </div>
@@ -65,28 +72,28 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Macario</span>
-                                <span> - 22/05/1996</span>
+                                <span> - 22/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Andre Silva</span>
-                                <span> - 13/05/1975</span>
+                                <span> - 13/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Gabriel Campos</span>
-                                <span> - 26/05/1981</span>
+                                <span> - 26/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Denis Rodrigues</span>
-                                <span> - 20/05/1985</span>
+                                <span> - 20/05</span>
                             </div>
                         </div>
                     </div>
@@ -95,70 +102,70 @@ export default function Birthdays() {
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Milton Doval</span>
-                                <span> - 23/05/1998</span>
+                                <span> - 23/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Eric Costa</span>
-                                <span> - 13/05/1986</span>
+                                <span> - 13/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Fabricio Silva</span>
-                                <span> - 05/05/1978</span>
+                                <span> - 05/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Anderson Vieira</span>
-                                <span> - 18/05/1988</span>
+                                <span> - 18/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Ângelo Neto</span>
-                                <span> - 11/05/1976</span>
+                                <span> - 11/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Elton</span>
-                                <span> - 22/05/1982</span>
+                                <span> - 22/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Guilherme Torres</span>
-                                <span> - 22/05/1982</span>
+                                <span> - 22/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Deiverson</span>
-                                <span> - 15/05/1996</span>
+                                <span> - 15/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Leonardo Santos</span>
-                                <span> - 13/05/1984</span>
+                                <span> - 13/05</span>
                             </div>
                         </div>
 
                         <div className="d-flex anv">
                             <div className="nome-anv d-flex">
                                 <span>Andre Serpa</span>
-                                <span> - 28/05/1973</span>
+                                <span> - 28/05</span>
                             </div>
                         </div>
                     </div>
