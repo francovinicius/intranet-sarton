c06549496d21114cd238e7c7d36d98761f50da79
Vinicius Franco Duarte <duartevini.dev@gmail.com>
2023-06-22 13:41:16 -0300

    add slides conta microsft

--- src/Pages/ContaMicrosoft/index.js
+++ src/Pages/ContaMicrosoft/index.js
@@ -2,7 +2,51 @@ export default function ContaMicrosoft ()
 {
     return (
         <div>
-            Teste
+            <section>
+            <h2 className="training-tittle">Passo a Passo: Criação de conta Microsoft</h2>
+
+        <div className="d-flex justify-content-center">
+            <div id="carouselExampleFade" className="carousel slide carousel-fade carousel-training" data-bs-ride="carousel">
+            <div className="carousel-inner">
+                <div className="carousel-item active">
+                    <img src="./assets/img/Conta-Microsoft/image-1.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-2.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-3.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-4.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-5.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-6.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-7.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-8.svg" className="d-block w-100" alt="..." />
+                </div>
+                <div className="carousel-item">
+                    <img src="./assets/img/Conta-Microsoft/image-9.svg" className="d-block w-100" alt="..." />
+                </div>
+            </div>
+            <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Voltar</span>
+            </button>
+            <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                <span className="visually-hidden">Avançar</span>
+            </button>
+        </div>
+        </div>
+        </section>
         </div>
     )
 }
